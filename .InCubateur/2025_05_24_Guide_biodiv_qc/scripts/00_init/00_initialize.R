cb3704c84098940c53d2e58fc4d33eb899d6c066
beausoleilmo <beausoleilmo@gmail.com>
2026-01-09 23:07:56 -0500

    propre

--- .InCubateur/2025_05_24_Guide_biodiv_qc/scripts/00_init/00_initialize.R
+++ .InCubateur/2025_05_24_Guide_biodiv_qc/scripts/00_init/00_initialize.R
@@ -5,17 +5,17 @@
 # date création: 2025-08-31
 # auteur: Marc-Olivier Beausoleil
 
-#### ____________####
-#### Lisez-moi --------
+# ____________####
+# Lisez-moi --------
 #   --> Préparation de l'environnement de travail
 #   --> Charger les progiciels R
 #   --> Charge les fonctions
 
-#### ____________####
+# ____________####
 # Chemin d'accès du projet ------
 
-# Préparation de l'environnement ------------------------------------------
-# Prendre le chemin d'accès actuel
+## Préparation de l'environnement ------------------------------------------
+# Prendre le chemin d'accès actuel avec getwd()
 # Mettre le chemin d'accès dans le dossier du projet
 if (!grepl(pattern = '2025_05_24_Guide_biodiv_qc', x = getwd())) {
   setwd('.InCubateur/2025_05_24_Guide_biodiv_qc')
@@ -28,47 +28,54 @@ mapply(FUN = dir.create,
        showWarnings = FALSE, 
        recursive = TRUE)
 
-#### ____________####
+# ____________####
 # Progiciels R ------------------------------------------------------------
 
-# install.packages(c("dplyr", "ggplot2", "sf", "terra",
-#                    "mapview", "tictoc",
-#                    "duckdb", "duckdbsf", "rgbif", "rinat", 
-#                    "jpeg",
-#                    "fs", "generics",
-#                    "leafem", "maps",
-#                    "sass", "servr",
+## Commande pour installer des progiciels au besoin -----
+# install.packages(c("dplyr", "ggplot2", 
+#                    "sf", "terra","mapview", 
+#                    "tictoc",
+#                    "duckdb", "duckdbsf", "rgbif", "rinat",
+#                    "jpeg", "fs", "generics",
+#                    "leafem", "maps", "sass", "servr",
 #                    "svglite", "tinytex", "promises"
 #                    ))
 
+
+## Progiciels en mémoire ---------------------------------------------------
 # Manipulation de données
-library(dplyr) # -> manipulation et préparation de données
-library(ggplot2) # -> graphiques pour visualisation de données
+library(dplyr)    # -> manipulation et préparation de données
+library(ggplot2)  # -> graphiques pour visualisation de données
 
 # Cartographie et géomatique
-library(sf) # -> manipulation spatiale et cartographie
-library(terra) # -> manipulation spatiale et cartographie (dont les raster)
-library(mapview) # -> cartes interactives pour visualisations rapides
+library(sf)       # -> manipulation spatiale et cartographie
+library(terra)    # -> manipulation spatiale et cartographie (dont les raster)
+library(mapview)  # -> cartes interactives pour visualisations rapides
 
 # Utilitaire
-library(tictoc) # -> minuteur pour mesurer le temps de long processus.
+library(tictoc)   # -> minuteur pour mesurer le temps de long processus.
 
 # Bases de données
-library(duckdb) # Interface pour les base de données
+library(duckdb)   # Interface pour les base de données
 library(duckdbfs) # Système de fichier de haute performance pour les base de données
 
-library(rgbif) # Lire les données GBIF 
-library(rinat) # Lire les données iNaturalist
+library(rgbif)    # Lire les données GBIF 
+library(rinat)    # Lire les données iNaturalist
 
+
+## Citations progiciels ----------------------------------------------------
 # Exporter les citations des progiciels R en fichier texte
 knitr::write_bib(.packages(),
                  "my_citations.bib")
 
-#### ____________####
+# ____________####
 # Charge les fonctions ----------------------------------------------------
 source(file = 'scripts/00_init/functions.R')
 
-# Définition de CRS pour projet ----------------------------------------------------
+# ____________####
+# Paramètres ----------------------------------------------------
+
+## Définition de CRS pour projet ----------------------------------------------------
 input_name = "NAD83 / Quebec Lambert"
 crs_32198 = readLines(con = "data/param_0/projetCRS.txt")
 
@@ -81,7 +88,7 @@ projetCRS = structure(
   class = "crs"
 )
 
-# Emprise Québec ----------------------------------------------------
+## Emprise Québec ----------------------------------------------------
 # Prépare l'emprise spatiale du Québec
 bbox_qc = structure(
   c(
