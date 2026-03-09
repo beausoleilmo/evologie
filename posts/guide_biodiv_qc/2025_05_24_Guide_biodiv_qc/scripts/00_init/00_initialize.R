1d356f1a05cfa2029918970cab4f18f6be1c5354
beausoleilmo <beausoleilmo@gmail.com>
2026-03-08 22:37:06 -0400

    MAJ site web.

--- posts/guide_biodiv_qc/2025_05_24_Guide_biodiv_qc/scripts/00_init/00_initialize.R
+++ posts/guide_biodiv_qc/2025_05_24_Guide_biodiv_qc/scripts/00_init/00_initialize.R
@@ -17,23 +17,25 @@
 ## Préparation de l'environnement ------------------------------------------
 # Prendre le chemin d'accès actuel avec getwd()
 # Mettre le chemin d'accès dans le dossier du projet
-if (!grepl(pattern = '2025_05_24_Guide_biodiv_qc', x = getwd())) {
-  setwd('.InCubateur/2025_05_24_Guide_biodiv_qc')
-}
+# if (!grepl(pattern = '2025_05_24_Guide_biodiv_qc', x = getwd())) {
+#   setwd('.InCubateur/2025_05_24_Guide_biodiv_qc')
+# }
 
 # Création de dossier de sortie
-part_fold = sprintf(c("partie_%d"), 1:3)
-mapply(FUN = dir.create, 
-       path = sprintf("output/%s", part_fold),
-       showWarnings = FALSE, 
-       recursive = TRUE)
+part_fold <- sprintf(c("partie_%d"), 1:3)
+mapply(
+  FUN = dir.create,
+  path = sprintf("output/%s", part_fold),
+  showWarnings = FALSE,
+  recursive = TRUE
+)
 
 # ____________####
 # Progiciels R ------------------------------------------------------------
 
 ## Commande pour installer des progiciels au besoin -----
-# install.packages(c("dplyr", "ggplot2", 
-#                    "sf", "terra","mapview", 
+# install.packages(c("dplyr", "ggplot2",
+#                    "sf", "terra","mapview",
 #                    "tictoc",
 #                    "duckdb", "duckdbsf", "rgbif", "rinat",
 #                    "jpeg", "fs", "generics",
@@ -44,49 +46,53 @@ mapply(FUN = dir.create,
 
 ## Progiciels en mémoire ---------------------------------------------------
 # Manipulation de données
-library(dplyr)    # -> manipulation et préparation de données
-library(ggplot2)  # -> graphiques pour visualisation de données
+library(dplyr) # -> manipulation et préparation de données
+library(ggplot2) # -> graphiques pour visualisation de données
 
 # Cartographie et géomatique
-library(sf)       # -> manipulation spatiale et cartographie
-library(terra)    # -> manipulation spatiale et cartographie (dont les raster)
-library(mapview)  # -> cartes interactives pour visualisations rapides
-library(leafem)   # -> Pour utilisation de mapview
-library(servr)    # -> Pour utilisation de mapview
-library(svglite)  # -> Pour utilisation de mapview
-library(units)    # -> manipulation d'unités de mesures
+library(sf) # -> manipulation spatiale et cartographie
+library(terra) # -> manipulation spatiale et cartographie (dont les raster)
+library(mapview) # -> cartes interactives pour visualisations rapides
+library(leafem) # -> Pour utilisation de mapview
+library(servr) # -> Pour utilisation de mapview
+library(svglite) # -> Pour utilisation de mapview
+library(units) # -> manipulation d'unités de mesures
 
 # Utilitaire
-library(tictoc)   # -> minuteur pour mesurer le temps de long processus.
+library(tictoc) # -> minuteur pour mesurer le temps de long processus.
 
 # Bases de données
-library(duckdb)   # Interface pour les base de données
+library(duckdb) # Interface pour les base de données
 library(duckdbfs) # Système de fichier de haute performance pour les base de données
 
-library(rgbif)    # Lire les données GBIF 
-library(rinat)    # Lire les données iNaturalist
-library(maps)     # -> dépendance de rinat
+library(rgbif) # Lire les données GBIF
+library(rinat) # Lire les données iNaturalist
+library(maps) # -> dépendance de rinat
 
+library(webshot2) # Enregistrer des cartes.
+# webshot::install_phantomjs Doit être installé
 
 ## Citations progiciels ----------------------------------------------------
 # Exporter les citations des progiciels R en fichier texte
-knitr::write_bib(
-  x = .packages(),
-  file = "citations_R_packages.bib")
-
+suppressMessages(
+  knitr::write_bib(
+    x = .packages(),
+    file = "citations_R_packages.bib"
+  )
+)
 # ____________####
 # Charge les fonctions ----------------------------------------------------
-source(file = 'scripts/00_init/functions.R')
+source(file = here::here("posts/guide_biodiv_qc/2025_05_24_Guide_biodiv_qc/scripts/00_init/functions.R"))
 
 # ____________####
 # Paramètres ----------------------------------------------------
 
 ## Définition de CRS pour projet ----------------------------------------------------
-input_name = "NAD83 / Quebec Lambert"
-crs_32198 = readLines(con = "data/param_0/projetCRS.txt")
+input_name <- "NAD83 / Quebec Lambert"
+crs_32198 <- readLines(con = here::here("posts/guide_biodiv_qc/2025_05_24_Guide_biodiv_qc/data/param_0/projetCRS.txt"))
 
 # Prépare le CRS pour toutes les couches
-projetCRS = structure(
+projetCRS <- structure(
   list(
     input = input_name,
     wkt = crs_32198
@@ -96,7 +102,7 @@ projetCRS = structure(
 
 ## Emprise Québec ----------------------------------------------------
 # Prépare l'emprise spatiale du Québec
-bbox_qc = structure(
+bbox_qc <- structure(
   c(
     xmin = -830291.429999985,
     ymin = 117964.150000002,
